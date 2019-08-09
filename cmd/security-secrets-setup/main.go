12dcb31795c54b2249ae8c4c5cdad45894bd6532
Jim Wang <yutsung.jim.wang@intel.com>
2019-08-09 09:28:53 -0700

    try 1: code refactor on main.go of merging pki-init onto pki-setup
    Signed-off-by: Jim Wang <yutsung.jim.wang@intel.com>

--- cmd/security-secrets-setup/main.go
+++ cmd/security-secrets-setup/main.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -26,21 +27,76 @@ import (
 	"github.com/edgexfoundry/edgex-go/internal/security/setup"
 	"github.com/edgexfoundry/edgex-go/internal/security/setup/certificates"
 	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+
+	option "github.com/edgexfoundry/edgex-go/internal/security/pkiinit/option"
 )
+
+type exit interface {
+	callExit(int)
+}
+
+type exitCode struct{}
+
+type optionDispatcher interface {
+	run() (int, error)
+}
+
+type pkiInitOptionDispatcher struct{}
+
+var exitInstance = newExit()
+var dispatcherInstance = newOptionDispatcher()
+var helpOpt bool
+var generateOpt bool
+var importOpt bool
+var cacheOpt bool
+var cacheCAOpt bool
+var configFile string
+
+func init() {
+	// define and register command line flags:
+	flag.BoolVar(&helpOpt, "h", false, "help message")
+	flag.BoolVar(&helpOpt, "help", false, "help message")
+	flag.StringVar(&configFile, "config", "", "specify JSON configuration file: /path/to/file.json")
+	flag.StringVar(&configFile, "c", "", "specify JSON configuration file: /path/to/file.json")
+	flag.BoolVar(&generateOpt, "generate", false, "to generate a new PKI from scratch")
+	flag.BoolVar(&importOpt, "import", false, " deploys a PKI from a cached PKI")
+	flag.BoolVar(&cacheOpt, "cache", false, "generates a fresh PKI exactly once then caches it for future use")
+	flag.BoolVar(&cacheCAOpt, "cacheca", false, "generates a fresh PKI exactly once then caches it with the CA keys presevered")
+}
 
 func main() {
 	start := time.Now()
-	var configFile string
 
-	flag.StringVar(&configFile, "config", "", "specify JSON configuration file: /path/to/file.json")
-	flag.StringVar(&configFile, "c", "", "specify JSON configuration file: /path/to/file.json")
 	flag.Usage = usage.HelpCallbackSecuritySetup
 	flag.Parse()
 
+	if helpOpt {
+		// as specified in the requirement, help option terminates with 0 exit status
+		flag.Usage()
+		exitInstance.callExit(0)
+		return
+	}
+
 	if configFile == "" {
 		flag.PrintDefaults()
-		os.Exit(1)
+		exitInstance.callExit(0)
+		return
 	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please specify option for security-secrets-setup.")
+		flag.Usage()
+		exitInstance.callExit(1)
+		return
+	}
+
+	statusCode, err := dispatcherInstance.run()
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	exitInstance.callExit(statusCode)
 
 	setup.Init()
 
@@ -85,6 +141,39 @@ func main() {
 		return
 	}
 	lc.Info("PKISetup complete", internal.LogDurationKey, time.Since(start).String())
+}
+
+func newExit() exit {
+	return &exitCode{}
+}
+
+func newOptionDispatcher() optionDispatcher {
+	return &pkiInitOptionDispatcher{}
+}
+
+func (code *exitCode) callExit(statusCode int) {
+	os.Exit(statusCode)
+}
+
+func setupPkiInitOption() (executor option.OptionsExecutor, status int, err error) {
+	opts := option.PkiInitOption{
+		GenerateOpt: generateOpt,
+		ImportOpt:   importOpt,
+		CacheOpt:    cacheOpt,
+		CacheCAOpt:  cacheCAOpt,
+	}
+
+	return option.NewPkiInitOption(opts)
+}
+
+func (dispatcher *pkiInitOptionDispatcher) run() (statusCode int, err error) {
+
+	optsExecutor, statusCode, err := setupPkiInitOption()
+	if err != nil {
+		return
+	}
+
+	return optsExecutor.ProcessOptions()
 }
 
 // TODO: ELIMINATE THIS ----------------------------------------------------------
